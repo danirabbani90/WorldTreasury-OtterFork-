48a5587aa191798600da64e2120f0329fb56d4ac
Dani R <35744462+danirabbani90@users.noreply.github.com>
2022-03-02 16:35:35 +0500

    updated Graph Links

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,6 @@
-export const THE_GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/otterclam/otterclam';
+// export const THE_GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/otterclam/otterclam';
+export const THE_GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/danirabbani90/world-treasury';
+
 
 export * from './blockchain';
 export * from './bonds';
